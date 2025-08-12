c3b905c434a33eed4a3fe65ec39764bc7b9b25a0
Luis Alvarez <johnlavz34@gmail.com>
2025-08-12 01:42:29 -0500

    applying standard way to import css modules in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Column from "./components/Column/Column";
 import { tasks } from "./data";
-import "./App.module.css";
+import styles from "./App.module.css";
 
 export default function App() {
   return (
     <>
-      <div className="app">
-        <div className="board">
+      <div className={styles.app}>
+        <div className={styles.board}>
           <Column title="To Do" tasks={tasks.todo} />
           <Column title="In Progress" tasks={tasks.inprogress} />
           <Column title="Done" tasks={tasks.done} />
