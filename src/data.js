a8f8e1d4bac548ce8dea0abee4cfa47ea208aadb
Luis Alvarez <johnlavz34@gmail.com>
2025-08-11 21:58:21 -0500

    Add initial task structure

---
+++ src/data.js
@@ -0,0 +1,5 @@
+export const tasks = {
+  todo: [{ id: "1", title: "Setup  project repository" }],
+  inprogress: [{ id: "2", title: "Create basic project folder" }],
+  done: [{ id: "3", title: "design homepage" }],
+};
