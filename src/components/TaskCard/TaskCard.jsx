fd6213dc55e8619bff54a7ae2290920e9957725a
Luis Alvarez <johnlavz34@gmail.com>
2025-08-12 01:44:17 -0500

    Aplying standard way to import css module in TaskCard.jsx

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,3 +1,4 @@
+import styles from "./TaskCard.module.css";
 export default function TaskCard({ task }) {
-  return <div className="task-card">{task.title}</div>;
+  return <div className={styles.taskCard}>{task.title}</div>;
 }
