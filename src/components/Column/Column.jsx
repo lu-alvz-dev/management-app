2428d6201d0c5686995233858c25909c00c16d59
Luis Alvarez <johnlavz34@gmail.com>
2025-08-12 01:33:42 -0500

    import styles from Column.module .css inside Column.jsx

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,10 +1,10 @@
 import TaskCard from "../TaskCard/TaskCard";
-import "./Column.module.css";
+import styles from "./Column.module.css";
 
 export default function Column({ title, tasks }) {
   return (
-    <div>
-      <h2>{title}</h2>
+    <div className={styles.column}>
+      <h2 className={styles.title}>{title}</h2>
       {tasks.length === 0 ? (
         <p>No tasks</p>
       ) : (
