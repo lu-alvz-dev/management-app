ec9f77cd63a8a524aa65582d35c9a81400b70971
Luis Alvarez <johnlavz34@gmail.com>
2025-08-12 20:48:33 -0500

    render AddTask components in Column - Passed task data from Column to AddTask.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import Column from "./components/Column/Column";
-import { tasks } from "./data";
+import { tasks as initialTasks } from "./data";
 import styles from "./App.module.css";
-
+import AddTask from "./components/AddTask/AddTask";
+import { useState } from "react";
 export default function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const addTask = (column, task) => {
+    setTasks((prev) => ({ ...prev, [column]: [...prev[column], task] }));
+  };
+
   return (
     <>
       <div className={styles.app}>
         <div className={styles.board}>
-          <Column title="To Do" tasks={tasks.todo} />
-          <Column title="In Progress" tasks={tasks.inprogress} />
-          <Column title="Done" tasks={tasks.done} />
+          <Column title="To Do" tasks={tasks.todo}>
+            <AddTask onAdd={(task) => addTask("todo", task)} />
+          </Column>
+          <Column title="In Progress" tasks={tasks.inprogress}>
+            <AddTask onAdd={(task) => addTask("inprogress", task)} />
+          </Column>
+          <Column title="Done" tasks={tasks.done}>
+            <AddTask onAdd={(task) => addTask("done", task)} />
+          </Column>
         </div>
       </div>
     </>
