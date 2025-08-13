2ceac63e2f3a9fc76ce971df5be58fd98fbb3384
Luis Alvarez <johnlavz34@gmail.com>
2025-08-12 19:39:50 -0500

    implement TaskCard functionality to get new task from input - add handleSubmit function - Add onchange for submit button

--- src/components/AddTask/AddTask.jsx
+++ src/components/AddTask/AddTask.jsx
@@ -1,7 +1,23 @@
-export default function AddTask() {
+import { useState } from "react";
+import nanoid from "nanoid";
+export default function AddTask({ onAdd }) {
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title.trim()) return;
+    onAdd({ id: nanoid(), title });
+    setTitle("");
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="New Task..." />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="New Task..."
+        value={title}
+        onchange={(e) => setTitle(e.target.value)}
+      />
       <button type="submit">+</button>
     </form>
   );
