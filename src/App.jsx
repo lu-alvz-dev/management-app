e8ffecce7fadb05aa77a5d02312c5b5a4808d789
Luis Alvarez <johnlavz34@gmail.com>
2025-08-15 00:51:23 -0500

    Add DndContext to activate drag and drop behavior

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { tasks as initialTasks } from "./data";
 import styles from "./App.module.css";
 import AddTask from "./components/AddTask/AddTask";
 import { useState } from "react";
+//import { DndContext } from "@dnd-kit/core"; to activate drag-and-drop behavior.
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import { arrayMove } from "@dnd-kit/sortable";
 export default function App() {
   const [tasks, setTasks] = useState(initialTasks);
   const addTask = (column, task) => {
@@ -12,17 +15,19 @@ export default function App() {
   return (
     <>
       <div className={styles.app}>
-        <div className={styles.board}>
-          <Column title="To Do" tasks={tasks.todo}>
-            <AddTask onAdd={(task) => addTask("todo", task)} />
-          </Column>
-          <Column title="In Progress" tasks={tasks.inprogress}>
-            <AddTask onAdd={(task) => addTask("inprogress", task)} />
-          </Column>
-          <Column title="Done" tasks={tasks.done}>
-            <AddTask onAdd={(task) => addTask("done", task)} />
-          </Column>
-        </div>
+        <DndContext collisionDetection={closestCenter}>
+          <div className={styles.board}>
+            <Column title="To Do" tasks={tasks.todo}>
+              <AddTask onAdd={(task) => addTask("todo", task)} />
+            </Column>
+            <Column title="In Progress" tasks={tasks.inprogress}>
+              <AddTask onAdd={(task) => addTask("inprogress", task)} />
+            </Column>
+            <Column title="Done" tasks={tasks.done}>
+              <AddTask onAdd={(task) => addTask("done", task)} />
+            </Column>
+          </div>
+        </DndContext>
       </div>
     </>
   );
