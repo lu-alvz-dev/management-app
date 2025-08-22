44b4006d592565b3aa5b9662e083b0d83d7c7746
Luis Alvarez <johnlavz34@gmail.com>
2025-08-22 16:54:41 -0500

    adding droppable functionality to column

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,14 +1,19 @@
 import TaskCard from "../TaskCard/TaskCard";
 import styles from "./Column.module.css";
+import { useDroppable } from "@dnd-kit/core";
 
-export default function Column({ title, tasks }) {
+export default function Column({ title, tasks, columnId, sortable }) {
+  // applying droppable functionality to column
+  const setNodeRef = useDroppable({ id: columnId });
   return (
-    <div className={styles.column}>
+    <div ref={setNodeRef} className={styles.column}>
       <h2 className={styles.title}>{title}</h2>
       {tasks.length === 0 ? (
         <p>No tasks</p>
       ) : (
-        tasks.map((task) => <TaskCard key={task.id} task={task} />)
+        tasks.map((task) => (
+          <TaskCard key={task.id} task={task} sortable={sortable} />
+        ))
       )}
     </div>
   );
