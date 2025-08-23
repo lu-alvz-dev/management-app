ec0dfa89f3751b7f8dcf4d7ab61b4c176aaef514
Luis Alvarez <johnlavz34@gmail.com>
2025-08-22 22:47:20 -0500

    Add drag animation functionality

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,4 +1,29 @@
 import styles from "./TaskCard.module.css";
-export default function TaskCard({ task }) {
-  return <div className={styles.taskCard}>{task.title}</div>;
+import { usesortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export default function TaskCard({ task, sortable = false }) {
+  if (!sortable) {
+    return <div className={styles.taskCard}>{task.title}</div>;
+  }
+
+  // Making the card draggable and sortable
+
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    usesortable({ id: task.id });
+
+  //adding dragging animation
+  const style = { transform: CSS.Transform.toString(transform), transition };
+
+  return (
+    <div
+      ref={setNodeRef}
+      style={style}
+      className={styles.taskCard}
+      {...attributes}
+      {...listeners}
+    >
+      {task.title}
+    </div>
+  );
 }
